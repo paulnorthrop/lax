df160b6f77b8492e5db890c0bafe1dfd24cb114d
Paul Northrop <p.northrop@ucl.ac.uk>
2018-10-24 16:43:19 +0100

    Correct default estfun method.

--- R/estfun.R
+++ R/estfun.R
@@ -1,6 +1,6 @@
 #' @export
-estfun.default <- function(x, ...) {
-  my_fn <- paste("logLikFn.", class(x), sep = "")
-  U <- numDeriv::jacobian(eval(as.name(my_fn)), x = coef(x), ...)
+estfun.default <- function(x, loglik_fn, ...) {
+  U <- numDeriv::jacobian(loglik_fn, x = coef(x), ...)
+  colnames(U) <- names(coef(x))
   return(U)
 }
