f89fb6903cde08e9a10e1eff5e9617a391eccee4
Paul Northrop <p.northrop@ucl.ac.uk>
2018-10-31 18:00:30 +0000

    Add logLik.logLikVec method.

--- R/methods.R
+++ R/methods.R
@@ -1,7 +1,32 @@
+#' Evaluate loglikelihood contributions from specific observations
+#'
+#' Generic function for calculating loglikelihood contributions from
+#' individual observations for a fitted model.
+#'
+#' @param object A fitted model object.
+#' @param ... Further arguments.
+#' @export
 logLikVec <- function(x, ...) {
   UseMethod("logLikVec")
 }
 
+#' @export
 alogLik <- function(x, ...) {
   UseMethod("alogLik")
+}
+
+#' Sum loglikelihood contributions from individual observations
+#'
+#' S3 logLik method for logLikVec objects
+#'
+#' @param object An object of class \code{"logLikVec"} return from a
+#'   \code{logLikVec} method.
+#' @param ... Further arguments.
+#' @export
+logLik.logLikVec <- function(object, ...) {
+  save_attributes <- attributes(object)
+  object <- sum(object)
+  attributes(object) <- save_attributes
+  class(object) <- "logLik"
+  return(object)
 }
