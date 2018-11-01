a494411a44375cfca45c8ffeea26ee5bbc7851e6
Paul Northrop <p.northrop@ucl.ac.uk>
2018-11-01 21:48:07 +0000

    Add tests.

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(oolax)
+
+test_check("oolax")
---
+++ tests/testthat/test-logLik.R
@@ -0,0 +1,21 @@
+context("logLik")
+
+# Check that logLik(object) and logLik(logLikVec(object)) agree
+
+if (requireNamespace("evd", quietly = TRUE)) {
+  library(evd)
+  # An example from the evd::fgev documentation
+  uvdata <- evd::rgev(100, loc = 0.13, scale = 1.1, shape = 0.2)
+  M1 <- evd::fgev(uvdata, nsloc = (-49:50)/100)
+  temp <- M1
+  class(temp) <- "evd_fgev"
+  # Note: evd::logLik.evd returns non-standard attributes (no nobs)
+  # Therefore, use expect_equivalent(), rather than expect_equal()
+  test_that("evd::fgev: logLik() vs. logLik(logLikVec)", {
+    testthat::expect_equivalent(logLik(M1), logLik(logLikVec(temp)))
+  })
+  # Check logLik.evd_fgev: trivially correct
+  test_that("evd::fgev: logLik() vs. logLik(logLikVec)", {
+    testthat::expect_equal(logLik(temp), logLik(logLikVec(temp)))
+  })
+}
