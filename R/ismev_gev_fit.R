c6a9b99275c56bf00390dbc7e0fc57e0d6d40eea
Paul Northrop <p.northrop@ucl.ac.uk>
2018-11-02 16:12:47 +0000

    Add regression case.

--- R/ismev_gev_fit.R
+++ R/ismev_gev_fit.R
@@ -12,20 +12,52 @@ logLikVec.ismev_gev <- function(object, pars = NULL, ...) {
   }
   n_pars <- length(pars)
   #
-  if (object$trans) {
-    stop("Covariates are not allowed")
+  if (object$trans & is.null(object$ydat)) {
+    stop("Covariate data are needed.  Refit the model using oolax::oogev.fit")
   }
-  # If trans = FALSE then there are no covariates and object$data contains
-  # the response data
-  mu <- pars[1]
-  sigma <- pars[2]
-  xi <- pars[3]
-  # Calculate the weighted loglikelihood contributions
-  if (sigma <= 0) {
-    val <- -Inf
+  if (!object$trans) {
+    # If trans = FALSE then there are no covariates and object$data contains
+    # the response data
+    mu <- pars[1]
+    sigma <- pars[2]
+    xi <- pars[3]
+    # Calculate the loglikelihood contributions
+    if (sigma <= 0) {
+      val <- -Inf
+    } else {
+      val <- revdbayes::dgev(object$data, loc = mu, scale = sigma,
+                             shape = xi, log = TRUE)
+    }
   } else {
-    val <- revdbayes::dgev(object$data, loc = mu, scale = sigma,
-                           shape = xi, log = TRUE)
+    # If trans = TRUE then there are covariates, object$xdat contains the
+    # response data and object$ydat the matrix of covariate data
+    # object$model is a list containing the columns of ydat for mu, sigma, xi
+    # Numbers of regression parameters for mu, sigma and xi
+    reg_pars <- sapply(object$model, length)
+    cum_reg_pars <- cumsum(reg_pars)
+    mu <- pars[1]
+    if (reg_pars[1] > 0) {
+      mu_reg <- pars[2:(1 + reg_pars[1])]
+      mu <- mu + object$ydat[, object$model[[1]], drop = FALSE] %*% mu_reg
+    }
+    sigma <- pars[2 + reg_pars[1]]
+    if (reg_pars[2] > 0) {
+      sigma_reg <- pars[(3 + reg_pars[1]):(2 + cum_reg_pars[1])]
+      sigma <- sigma + object$ydat[, object$model[[2]], drop = FALSE] %*%
+        sigma_reg
+    }
+    xi <- pars[3 + cum_reg_pars[2]]
+    if (reg_pars[3] > 0) {
+      xi_reg <- pars[(4 + cum_reg_pars[2]):(3 + cum_reg_pars[3])]
+      xi <- xi + object$ydat[, object$model[[3]], drop = FALSE] %*% xi_reg
+    }
+    # Calculate the loglikelihood contributions
+    if (any(sigma <= 0)) {
+      val <- -Inf
+    } else {
+      val <- revdbayes::dgev(object$xdat, loc = mu, scale = sigma,
+                             shape = xi, log = TRUE)
+    }
   }
   return(val)
 }
