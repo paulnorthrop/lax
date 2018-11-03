adb5d745b35788ed89f2f82e5d6319680d62c563
Paul Northrop <p.northrop@ucl.ac.uk>
2018-11-03 21:29:44 +0000

    Add logLikVec method for ismev::pp.fit().

--- R/evd_fgev.R
+++ R/evd_fgev.R
@@ -7,11 +7,13 @@ logLikVec.evd_fgev <- function(object, pars = NULL, ...) {
   if (!missing(...)) {
     warning("extra arguments discarded")
   }
-  # If the parameter estimates have not been provided in pars then extract
+  # If the free parameter values have not been provided in pars then extract
   # them from the fitted object
   if (is.null(pars)) {
-    pars <- coef(object)
+    pars <- coef(object, complete = FALSE)
   }
+  # Add fixed parameter values (if any)
+#  pars <- c(pars, object$fixed)
   n_pars <- length(pars)
   mu <- pars[1]
   sigma <- pars[n_pars - 1]
@@ -40,8 +42,31 @@ nobs.evd_fgev <- function(object, ...) {
 }
 
 #' @export
-coef.evd_fgev <- function(object, ...) {
-  return(object$estimate)
+coef.evd_fgev <- function(object, complete = FALSE, ...) {
+  if (complete) {
+    val <- object$param
+  } else {
+    val <- object$estimate
+  }
+  return(val)
+}
+
+#' @export
+vcov.evd_fgev <- function(object, complete = FALSE, ...) {
+  # Variance-covariance matrix for the free parameters
+  vc <- object$var.cov
+  free_pars <- names(coef(object, complete = FALSE))
+  dimnames(vc) <- list(free_pars, free_pars)
+  if (complete) {
+    all_pars <- names(coef(object, complete = TRUE))
+    np <- length(all_pars)
+    dummy <- matrix(NA, np, np)
+    which_free <- which(all_pars %in% free_pars)
+    dummy[which_free, which_free] <- vc
+    vc <- dummy
+    dimnames(vc) <- list(all_pars, all_pars)
+  }
+  return(vc)
 }
 
 #' @export
@@ -62,7 +87,11 @@ nobs.gev <- function(object, ...) {
 }
 
 #' @export
-coef.gev <- function(object, ...) {
-  return(object$estimate)
+coef.gev <- function(object, complete = FALSE, ...) {
+  if (complete) {
+    val <- object$param
+  } else {
+    val <- object$estimate
+  }
+  return(val)
 }
-
