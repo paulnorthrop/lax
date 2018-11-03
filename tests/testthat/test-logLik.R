34ddb49da37ebff8b16b9b1f6d243cd9a81dd887
Paul Northrop <p.northrop@ucl.ac.uk>
2018-11-03 16:04:53 +0000

    Add model = "pp" test for evd::fpot().

--- tests/testthat/test-logLik.R
+++ tests/testthat/test-logLik.R
@@ -28,7 +28,23 @@ if (requireNamespace("evd", quietly = TRUE)) {
   library(evd)
   # An example from the evd::fpot documentation
   uvdata <- evd::rgpd(100, loc = 0, scale = 1.1, shape = 0.2)
+
+  # model = "gpd"
   M1 <- evd::fpot(uvdata, 1)
+  temp <- M1
+  class(temp) <- "evd_fpot"
+  # Note: evd::logLik.evd returns non-standard attributes (no nobs)
+  # Therefore, use expect_equivalent(), rather than expect_equal()
+  test_that("evd::fgev: logLik() vs. logLik(logLikVec)", {
+    testthat::expect_equivalent(logLik(M1), logLik(logLikVec(temp)))
+  })
+  # Check logLik.evd_fgev: trivially correct
+  test_that("evd::fgev: logLik() vs. logLik(logLikVec)", {
+    testthat::expect_equal(logLik(temp), logLik(logLikVec(temp)))
+  })
+
+  # model = "pp"
+  M1 <- evd::fpot(uvdata, 1, model = "pp")
   temp <- M1
   class(temp) <- "evd_fpot"
   # Note: evd::logLik.evd returns non-standard attributes (no nobs)
