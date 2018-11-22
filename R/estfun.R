7c63076085b86b5c3f4aac25f465691d810b5708
Paul Northrop <p.northrop@ucl.ac.uk>
2018-11-22 21:49:45 +0000

    Create specific estfun methods.

--- R/estfun.R
+++ R/estfun.R
@@ -1,5 +1,105 @@
+# Create a default estfun method and, for safety, create individual methods
+# for all the classes currently involved in oolax.  At the moment they all
+# use numDeriv::jacobian() to do the calculation
+
 #' @export
 estfun.default <- function(x, loglik_fn, ...) {
+  U <- numDeriv::jacobian(loglik_fn, x = coef(x), ...)
+  colnames(U) <- names(coef(x))
+  return(U)
+}
+
+# evd
+
+#' @export
+estfun.evd_fgev <- function(x, loglik_fn, ...) {
+  U <- numDeriv::jacobian(loglik_fn, x = coef(x), ...)
+  colnames(U) <- names(coef(x))
+  return(U)
+}
+
+#' @export
+estfun.evd_fpot <- function(x, loglik_fn, ...) {
+  U <- numDeriv::jacobian(loglik_fn, x = coef(x), ...)
+  colnames(U) <- names(coef(x))
+  return(U)
+}
+
+# evir
+
+#' @export
+estfun.evir_gev <- function(x, loglik_fn, ...) {
+  U <- numDeriv::jacobian(loglik_fn, x = coef(x), ...)
+  colnames(U) <- names(coef(x))
+  return(U)
+}
+
+#' @export
+estfun.evir_gpd <- function(x, loglik_fn, ...) {
+  U <- numDeriv::jacobian(loglik_fn, x = coef(x), ...)
+  colnames(U) <- names(coef(x))
+  return(U)
+}
+
+#' @export
+estfun.evir_pot <- function(x, loglik_fn, ...) {
+  U <- numDeriv::jacobian(loglik_fn, x = coef(x), ...)
+  colnames(U) <- names(coef(x))
+  return(U)
+}
+
+# extRemes
+
+#' @export
+estfun.fevd <- function(x, loglik_fn, ...) {
+  U <- numDeriv::jacobian(loglik_fn, x = coef(x), ...)
+  colnames(U) <- names(coef(x))
+  return(U)
+}
+
+# fExtremes
+
+#' @export
+estfun.fExtremes_gev <- function(x, loglik_fn, ...) {
+  U <- numDeriv::jacobian(loglik_fn, x = coef(x), ...)
+  colnames(U) <- names(coef(x))
+  return(U)
+}
+
+#' @export
+estfun.fExtremes_gpd <- function(x, loglik_fn, ...) {
+  U <- numDeriv::jacobian(loglik_fn, x = coef(x), ...)
+  colnames(U) <- names(coef(x))
+  return(U)
+}
+
+# ismev
+
+#' @export
+estfun.ismev_gev <- function(x, loglik_fn, ...) {
+  U <- numDeriv::jacobian(loglik_fn, x = coef(x), ...)
+  colnames(U) <- names(coef(x))
+  return(U)
+}
+
+#' @export
+estfun.ismev_pp <- function(x, loglik_fn, ...) {
+  U <- numDeriv::jacobian(loglik_fn, x = coef(x), ...)
+  colnames(U) <- names(coef(x))
+  return(U)
+}
+
+#' @export
+estfun.ismev_gpd <- function(x, loglik_fn, ...) {
+  U <- numDeriv::jacobian(loglik_fn, x = coef(x), ...)
+  colnames(U) <- names(coef(x))
+  return(U)
+}
+
+# texmex
+
+#' @export
+estfun.texmex_evmOpt <- function(x, loglik_fn, ...) {
   U <- numDeriv::jacobian(loglik_fn, x = coef(x), ...)
   colnames(U) <- names(coef(x))
   return(U)
