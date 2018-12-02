4ff26da32e647b1703235bd3beb5fbfe8500193c
Paul Northrop <p.northrop@ucl.ac.uk>
2018-12-02 19:23:46 +0000

    Alphbetical order.

--- R/logLik_functions.R
+++ R/logLik_functions.R
@@ -5,22 +5,22 @@
 # evd::fextreme() : c("extreme","evd")
 # evd::forder() : c("extreme","evd")
 
-# fExtremes::gevFit() : "fGEVFIT"
-# fExtremes::gpdFit() : fGPDFIT
-
 # evir::gev() "gev"
 # evir::gpd() "gpd"
 
-# texmex::evm() : "evmOpt" (gev, gpd and egp3)
+# extRemes::fevd() : "fevd"
+
+# fExtremes::gevFit() : "fGEVFIT"
+# fExtremes::gpdFit() : fGPDFIT
 
 # ismev::gev.fit : "gev.fit"
 # ismev::gpd.fit : "gpd.fit"
 
-# extRemes::fevd() : "fevd"
+# POT::fitgpd() : c("uvpot", "pot")
+
+# texmex::evm() : "evmOpt" (gev, gpd and egp3)
 
 # evmix::fgpd() : "list"   Oh dear!
-
-# POT::fitgpd() : c("uvpot", "pot")
 
 # mev::gp.fit() : "gpd"  (clash with evir::gpd())
 # mev::egp.fit() : "list"  Oh dear
