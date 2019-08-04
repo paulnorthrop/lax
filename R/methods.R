e69acb4d6afa83677e8ed652d57cb3940178e960
Paul Northrop <p.northrop@ucl.ac.uk>
2019-08-04 11:39:49 +0100

    Document the correct argument name

--- R/methods.R
+++ R/methods.R
@@ -3,7 +3,7 @@
 #' Generic function for calculating loglikelihood contributions from
 #' individual observations for a fitted model.
 #'
-#' @param object A fitted model object.
+#' @param x A fitted model object.
 #' @param ... Further arguments.
 #' @export
 logLikVec <- function(x, ...) {
