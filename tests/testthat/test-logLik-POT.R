7f0c2cc4f97e7d45b107d390a2e93c7b0aaad554
Paul Northrop <p.northrop@ucl.ac.uk>
2019-08-09 07:59:57 +0100

    Test POT

---
+++ tests/testthat/test-logLik-POT.R
@@ -0,0 +1,30 @@
+context("logLik, evd package")
+
+# Check that logLik(object) and logLik(logLikVec(object)) agree
+
+if (requireNamespace("POT", quietly = TRUE)) {
+  library(POT)
+
+  # POT::fitgpd
+
+  # An example from the POT::fitgpd documentation.
+  set.seed(4082019)
+  x <- POT::rgpd(200, 1, 2, 0.25)
+  fit <- POT::fitgpd(x, 1, "mle")
+  temp <- fit
+  adj_fit <- alogLik(fit)
+  class(temp) <- "POT_fitgpd"
+
+  test_that("POT::fitgpd: logLik() vs. logLik(logLikVec)", {
+    testthat::expect_equivalent(logLik(fit), logLik(logLikVec(temp)))
+  })
+  # Check that alogLik also returnd the correct maximised log-likelihood
+  test_that("POT::fitgpd: logLik() vs. logLik(logLikVec)", {
+    testthat::expect_equivalent(logLik(fit), logLik(adj_fit))
+  })
+  # Check logLik.POT_fitgpd: trivially correct
+  test_that("POT::fitgpd: logLik() vs. logLik(logLikVec)", {
+    testthat::expect_equal(logLik(temp), logLik(logLikVec(temp)))
+  })
+
+}
