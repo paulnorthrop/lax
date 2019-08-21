8945e33ea48c2d30611b1d08b9e83e4b70548383
Paul Northrop <p.northrop@ucl.ac.uk>
2019-08-21 23:42:19 +0100

    lax docs

--- R/lax.R
+++ R/lax.R
@@ -2,40 +2,31 @@
 #'
 #' Performs adjusted inferences based on model objects fitted, using maximum
 #' likelihood estimation, by the extreme value analysis packages
-#' \href{https://cran.r-project.org/package=evd}{evd}
-#' \href{https://cran.r-project.org/package=evir}{evir}
-#' \href{https://cran.r-project.org/package=extRemes}{extRemes}
-#' \href{https://cran.r-project.org/package=fExtremes}{fExtremes}
-#' \href{https://cran.r-project.org/package=ismev}{ismev}
-#' \href{https://cran.r-project.org/package=POT}{POT}
+#' \href{https://cran.r-project.org/package=evd}{evd},
+#' \href{https://cran.r-project.org/package=evir}{evir},
+#' \href{https://cran.r-project.org/package=extRemes}{extRemes},
+#' \href{https://cran.r-project.org/package=fExtremes}{fExtremes},
+#' \href{https://cran.r-project.org/package=ismev}{ismev},
+#' \href{https://cran.r-project.org/package=POT}{POT} and
 #' \href{https://cran.r-project.org/package=texmex}{texmex}.
+#' Univariate extreme value models, including regression models, are supported.
 #' Adjusted standard errors and an adjusted loglikelihood are provided,
 #' using the
 #' \href{https://cran.r-project.org/package=chandwich}{chandwich package}
 #'  and the object-oriented features of the
 #'  \href{https://cran.r-project.org/package=sandwich}{sandwich package}.
-#' Adjusted standard errors and an adjusted loglikelihood are provided.
 #' The adjustment is based on a robust sandwich estimator of the parameter
-#' covariance matrix, based on the methodology in Chandler and Bate (2007)
-#' <doi:10.1093/biomet/asm015>. This can be used for cluster correlated data
+#' covariance matrix, based on the methodology in Chandler and Bate (2007).
+#' This can be used for cluster correlated data
 #' when interest lies in the parameters of the marginal distributions, or for
 #' performing inferences that are robust to certain types of model
 #' misspecification.  Univariate extreme value models, including regression
-#' models, are supported.  As a byproduct, generic S3 logLik, coef, vcov and
-#' nobs methods are provided, where these are not already provided in the
-#' original package.
-#' @details Main function is \code{\link{alogLik}}
-#' \code{\link[lax]{evd}},
-#' \code{\link[lax]{evir}},
-#' \code{\link[lax]{extRemes}},
-#' \code{\link[lax]{fExtremes}},
-#' \code{\link[lax]{ismev}},
-#' \code{\link[lax]{POT}},
-#' \code{\link[lax]{texmex}}.
+#' models, are supported.
+#' @details Main function is \code{\link{alogLik}}, which works in an
+#' object-oriented way, operating on fitted model objects.
 #'
-#' See Chandler and Bate (2007) for full details and
-#' \code{vignette("an-overview-of-lax-vignette", package = "lax")} for an
-#' overview of the package.
+#' See \code{vignette("lax-vignette", package = "lax")} for an overview of the
+#' package.
 #' @references Berger S., Graham N., Zeileis A. (2017). Various Versatile
 #'   Variances: An Object-Oriented Implementation of Clustered Covariances in R.
 #'   Technical Report 2017-12, Working Papers in Economics and Statistics,
@@ -43,21 +34,30 @@
 #'   Innsbruck. \url{http://EconPapers.RePEc.org/RePEc:inn:wpaper:2017-12}.
 #' @references Chandler, R. E. and Bate, S. (2007). Inference for clustered
 #'   data using the independence loglikelihood. \emph{Biometrika},
-#'   \strong{94}(1), 167-183. \url{http://dx.doi.org/10.1093/biomet/asm015}
+#'   \strong{94}(1), 167-183. \url{http://doi.org/10.1093/biomet/asm015}
+#' @references Gilleland, E. and Katz, R. W. (2016). extRemes 2.0: An Extreme
+#'   Value Analysis Package in R. \emph{Journal of Statistical Software},
+#'   \strong{72}(8), 1-39. \url{http://doi.org/10.18637/jss.v072.i08}
 #' @references Northrop, P. J. and Chandler, R. E. (2018).
 #'   chandwich: Chandler-Bate Sandwich Loglikelihood Adjustment. R package
 #'   version 1.1. \url{https://CRAN.R-project.org/package=chandwich}.
 #' @references Pfaff, B. and McNeil, A. (2018). evir: Extreme Values in R.
 #'   R package version 1.7-4. \url{https://CRAN.R-project.org/package=evir}
+#' @references Ribatet, M. and Dutang, C. (2019). POT: Generalized Pareto
+#'   Distribution and Peaks Over Threshold. R package version 1.1-7.
+#'   \url{https://CRAN.R-project.org/package=POT}
 #' @references Southworth, H., Heffernan, J. E. and Metcalfe, P. D. (2017).
 #'   texmex: Statistical modelling of extreme values. R package version 2.4.
 #'   \url{https://CRAN.R-project.org/package=texmex}.
 #' @references Stephenson, A. G. evd: Extreme Value Distributions.
 #'   \emph{R News}, \strong{2}(2):31-32, June 2002.
-#'   \url{ttps://CRAN.R-project.org/doc/Rnews/}
+#'   \url{https://CRAN.R-project.org/doc/Rnews/}
 #' @references Stephenson, A. G., Heffernan, J. E. and Gilleland, E. (2018).
 #'   ismev: An Introduction to Statistical Modeling of Extreme Values.
 #'   R package version 1.42. \url{https://CRAN.R-project.org/package=ismev}.
+#' @references Wuertz, D., Setz, T. and Chalabi, Y. (2017). fExtremes:
+#'   Rmetrics - Modelling Extreme Events in Finance. R package version
+#'   3042.82. \url{https://CRAN.R-project.org/package=fExtremes}
 #' @references Zeileis A. (2004). Econometric Computing with HC and HAC
 #'   Covariance Matrix Estimators. \emph{Journal of Statistical Software},
 #'   \strong{11}(10), 1-17. \url{http://doi.org/10.18637/jss.v011.i10}.
