59367879cd68e2628ec66f21f215baa72925e803
Paul Northrop <p.northrop@ucl.ac.uk>
2019-08-21 15:17:36 +0100

    Move functions from return_level.R

--- R/lax-internal.R
+++ R/lax-internal.R
@@ -74,3 +74,128 @@ ismev_ppp <- function (a, npy) {
   xi <- a[3]
   c(la, sc, xi)
 }
+
+#' @keywords internal
+#' @rdname lax-internal
+return_level_gev <- function(x, m, level, npy, prof, inc, type) {
+  # MLE and symmetric conf% CI for the return level
+  rl_sym <- gev_rl_CI(x, m, level, npy, type)
+  if (!prof) {
+    return(list(rl_sym = rl_sym, rl_prof = NULL))
+  }
+  temp <- gev_rl_prof(x, m, level, npy, inc, type, rl_sym)
+  return(list(rl_sym = rl_sym, rl_prof = temp$rl_prof, max_loglik = logLik(x),
+              crit = temp$crit, for_plot = temp$for_plot))
+}
+
+#' @keywords internal
+#' @rdname lax-internal
+gev_rl_prof <- function(x, m, level, npy, inc, type, rl_sym) {
+  if (is.null(inc)) {
+    inc <- (rl_sym["upper"] - rl_sym["lower"]) / 100
+  }
+  p <- 1 / (m * npy)
+  # Calculates the negated profile loglikelihood of the m-year return level
+  gev_neg_prof_loglik <- function(a, xp) {
+    if (a[1] <= 0) {
+      return(10 ^ 10)
+    }
+    mu <- xp - revdbayes::qgev(1 - p, loc = 0, scale = a[1], shape = a[2])
+    gev_pars <- c(mu, a[1:2])
+    return(-x(gev_pars))
+  }
+  rl_mle <- rl_sym["mle"]
+  max_loglik <- attr(x, "max_loglik")
+  conf_line <- max_loglik - 0.5 * stats::qchisq(level, 1)
+  v1 <- v2 <- x1 <- x2 <- NULL
+  x2[1] <- x1[1] <- rl_mle
+  v2[1] <- v1[1] <- max_loglik
+  #
+  # Starting from the MLE, we search upwards and downwards until we pass the
+  # cutoff for the 100level% confidence interval
+  #
+  ### Upper tail ...
+  xp <- rl_mle
+  my_val <- max_loglik
+  ii <- 1
+  sol <- attr(x, "MLE")[2:3]
+  while (my_val > conf_line){
+    xp <- xp + inc
+    opt <- stats::optim(sol, gev_neg_prof_loglik, method = "BFGS", xp = xp)
+    sol <- opt$par
+    ii <- ii + 1
+    x2[ii] <- xp
+    v2[ii] <- -opt$value
+    my_val <- v2[ii]
+  }
+  sol_up <- sol
+  ### Lower tail ...
+  xp <- rl_mle
+  my_val <- max_loglik
+  ii <- 1
+  sol <- attr(x, "MLE")[2:3]
+  while (my_val > conf_line){
+    xp <- xp - inc
+    opt <- stats::optim(sol, gev_neg_prof_loglik, method = "BFGS", xp = xp)
+    sol <- opt$par
+    ii <- ii + 1
+    x1[ii] <- xp
+    v1[ii] <- -opt$value
+    my_val <- v1[ii]
+  }
+  sol_low <- sol
+  #
+  # Find the limits of the confidence interval
+  #
+  prof_lik <- c(rev(v1), v2)
+  ret_levs <- c(rev(x1), x2)
+  # Find where the curve crosses conf_line
+  temp <- diff(prof_lik - conf_line > 0)
+  # Find the upper limit of the confidence interval
+  loc <- which(temp == -1)
+  x1 <- ret_levs[loc]
+  x2 <- ret_levs[loc + 1]
+  y1 <- prof_lik[loc]
+  y2 <- prof_lik[loc + 1]
+  up_lim <- x1 + (conf_line - y1) * (x2 - x1) / (y2 - y1)
+  # Find the lower limit of the confidence interval
+  loc <- which(temp == 1)
+  x1 <- ret_levs[loc]
+  x2 <- ret_levs[loc+1]
+  y1 <- prof_lik[loc]
+  y2 <- prof_lik[loc+1]
+  low_lim <- x1 + (conf_line - y1) * (x2 - x1) / (y2 - y1)
+  rl_prof <- c(lower = low_lim, rl_mle, upper = up_lim)
+  return(list(rl_prof = rl_prof, crit = conf_line,
+              for_plot = cbind(ret_levs = ret_levs, prof_loglik = prof_lik)))
+}
+
+#' @keywords internal
+#' @rdname lax-internal
+gev_rl_CI <- function (x, m, level, npy, type){
+  mle <- attr(x, "MLE")
+  mu <- mle[1]
+  sigma <- mle[2]
+  xi <- mle[3]
+  if (type == "none") {
+    mat <- attr(x, "VC")
+  } else {
+    mat <- attr(x, "adjVC")
+  }
+  p <- 1 / (m * npy)
+  rl_mle <- revdbayes::qgev(p, loc = mu, scale = sigma, shape = xi,
+                            lower.tail = FALSE)
+  yp <- -log(1 - p)
+  delta <- matrix(0, 3, 1)
+  delta[1,] <- 1
+  delta[2,] <- revdbayes::qgev(p, loc = 0, scale = 1, shape = xi,
+                               lower.tail = FALSE)
+  delta[3,] <- sigma * box_cox_deriv(yp, lambda = -xi)
+  rl_var <- t(delta) %*% mat %*% delta
+  rl_se <- sqrt(rl_var)
+  z_val <- stats::qnorm(1 - (1 - level) / 2)
+  rl_lower <- rl_mle - z_val * rl_se
+  rl_upper <- rl_mle + z_val * rl_se
+  res <- c(lower = rl_lower, mle = rl_mle, upper = rl_upper)
+  return(res)
+}
