6cb765577c4842a8eadd2bec05b8f8f31cf2e327
Paul Northrop <p.northrop@ucl.ac.uk>
2019-08-22 01:12:11 +0100

    Add tests

--- tests/testthat/test-logLik-texmex.R
+++ tests/testthat/test-logLik-texmex.R
@@ -30,7 +30,7 @@ if (got_texmex) {
   })
 
   # texmex::evm, EGP3
-  mod <- evm(rain, th = 30, family = egp3)
+  mod <- texmex::evm(rain, th = 30, family = egp3)
   temp <- mod
   adj_mod <- alogLik(mod)
   class(temp) <- "texmex_evmOpt"
@@ -48,7 +48,7 @@ if (got_texmex) {
   })
 
   # GP
-  mod <- evm(rain, th = 30)
+  mod <- texmex::evm(rain, th = 30)
   temp <- mod
   adj_mod <- alogLik(mod)
   class(temp) <- "texmex_evmOpt"
@@ -62,6 +62,55 @@ if (got_texmex) {
   })
   # Check logLik.texmex_evmOpt, EGP3: trivially correct
   test_that("texmex::evm, EGP3: logLik() vs. logLik(logLikVec)", {
+    testthat::expect_equal(logLik(temp), logLik(logLikVec(temp)))
+  })
+
+  # GEV regression
+  # An example from page 113 of Coles (2001)
+  if (got_ismev) {
+    library(ismev)
+    data(fremantle)
+    new_fremantle <- fremantle
+    # Set year 1897 to 1 for consistency with page 113 of Coles (2001)
+    new_fremantle[, "Year"] <- new_fremantle[, "Year"] - 1896
+    mod <- texmex::evm(y = SeaLevel, data = new_fremantle, family = gev,
+                       mu = ~ Year + SOI)
+    temp <- mod
+    adj_mod <- alogLik(mod)
+    class(temp) <- "texmex_evmOpt"
+
+    test_that("texmex::evm, GEV, reg: logLik() vs. logLik(logLikVec)", {
+      testthat::expect_equivalent(logLik(mod), logLik(logLikVec(temp)))
+    })
+    # Check that alogLik also returned the correct maximised log-likelihood
+    test_that("texmex::evm, GEV, reg: logLik() vs. logLik(logLikVec)", {
+      testthat::expect_equivalent(logLik(mod), logLik(adj_mod))
+    })
+    # Check logLik.texmex_evmOpt, EGP3: trivially correct
+    test_that("texmex::evm, GEV, reg: logLik() vs. logLik(logLikVec)", {
+      testthat::expect_equal(logLik(temp), logLik(logLikVec(temp)))
+    })
+  }
+
+  # GP regression
+  # An example from page 119 of Coles (2001)
+  n_rain <- length(rain)
+  rain_df <- data.frame(rain = rain, time = 1:n_rain / n_rain)
+  mod <- texmex::evm(y = rain, data = rain_df, family = gpd, th = 30,
+                     phi = ~ time)
+  temp <- mod
+  adj_mod <- alogLik(mod)
+  class(temp) <- "texmex_evmOpt"
+
+  test_that("texmex::evm, GP, reg: logLik() vs. logLik(logLikVec)", {
+    testthat::expect_equivalent(logLik(mod), logLik(logLikVec(temp)))
+  })
+  # Check that alogLik also returned the correct maximised log-likelihood
+  test_that("texmex::evm, GP, reg: logLik() vs. logLik(logLikVec)", {
+    testthat::expect_equivalent(logLik(mod), logLik(adj_mod))
+  })
+  # Check logLik.texmex_evmOpt, EGP3: trivially correct
+  test_that("texmex::evm, GP, reg: logLik() vs. logLik(logLikVec)", {
     testthat::expect_equal(logLik(temp), logLik(logLikVec(temp)))
   })
 }
