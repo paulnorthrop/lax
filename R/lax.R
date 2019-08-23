25a03b43fc8095c9d0715a16a0b18cde89a74c81
Paul Northrop <p.northrop@ucl.ac.uk>
2019-08-23 21:51:19 +0100

    Add ow data

--- R/lax.R
+++ R/lax.R
@@ -79,3 +79,24 @@
 #' @importFrom stats nobs vcov coef logLik
 #' @importFrom graphics plot
 NULL
+
+#' Oxford and Worthing annual maximum temperatures
+#'
+#' Annual maximum temperatures at Oxford and Worthing (England), for the
+#' period 1901 to 1980.
+#'
+#' @format A dataframe with 80 rows and 4 columns.
+#'   \itemize{
+#'     \item{Column 1, \code{temp}: }{annual maximum temperatures in degrees
+#'       Fahrenheit.}
+#'     \item{Column 2, \code{year}: }{year in which the maximum was recorded.}
+#'     \item{Column 3, \code{name}: }{name of location, "oxford" or "worthing"}
+#'     \item{Column 4, \code{loc}: }{location: 1 for "oxford", -1 for
+#'       "worthing"}
+#'  }
+#' @source Tabony, R. C. (1983) Extreme value analysis in meteorology.
+#'  \emph{The Meteorological Magazine}, \strong{112}, 77-98.
+#' @references Chandler, R. E. and Bate, S. (2007). Inference for clustered
+#'   data using the independence loglikelihood. \emph{Biometrika},
+#'   \strong{94}(1), 167-183. \url{http://dx.doi.org/10.1093/biomet/asm015}
+"ow"
