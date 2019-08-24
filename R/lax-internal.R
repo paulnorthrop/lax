f85596dc88de1f757b5eda610d544efb2945161d
Paul Northrop <p.northrop@ucl.ac.uk>
2019-08-24 14:27:04 +0100

    Move box_cox_deriv() to lax-internal.R

--- R/lax-internal.R
+++ R/lax-internal.R
@@ -204,3 +204,35 @@ gev_rl_CI <- function (x, m, level, npy, type){
   res <- c(lower = rl_lower, mle = rl_mle, upper = rl_upper, se = rl_se)
   return(res)
 }
+
+# ====================== box_cox_deriv ==========================
+
+#' @keywords internal
+#' @rdname lax-internal
+box_cox_deriv <- function (x, lambda = 1, lambda_tol = 1e-6,
+                           poly_order = 3) {
+  #
+  # Computes the derivative with respect to lambda the Box-Cox
+  # transformation.
+  #
+  # Args:
+  #   x          : A numeric vector. (Positive) values to be Box-Cox
+  #                transformed.
+  #   lambda     : A numeric scalar.  Transformation parameter.
+  #   lambda_tol : A numeric scalar.  For abs(lambda) < lambda.tol use
+  #                a Taylor series expansion.
+  #   poly_order : order of Taylor series polynomial in lambda used as
+  #                an approximation if abs(lambda) < lambda.tol
+  #
+  # Returns:
+  #   A numeric vector.  The derivative with respect to lambda of
+  #     (x^lambda - 1) / lambda
+  #
+  if (abs(lambda) > lambda_tol) {
+    retval <- (lambda * x ^ lambda * log(x) - x ^ lambda + 1) / lambda ^ 2
+  } else {
+    i <- 0:poly_order
+    retval <- sum(log(x) ^ (i + 2) * lambda ^ i / ((i + 2) * factorial(i)))
+  }
+  return(retval)
+}
