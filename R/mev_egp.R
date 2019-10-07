2983935ed9856c14173b5b06927523b89cf25eea
Paul Northrop <p.northrop@ucl.ac.uk>
2019-10-07 17:59:47 +0100

    logLikVec method for mev_egp

---
+++ R/mev_egp.R
@@ -0,0 +1,35 @@
+# =============================== mev::fit.egp ============================== #
+
+# Method for class mev_egp
+
+#' @export
+logLikVec.mev_egp <- function(object, pars = NULL, ...) {
+  if (!missing(...)) {
+    warning("extra arguments discarded")
+  }
+  # If the parameter estimates have not been provided in pars then extract
+  # them from the fitted object
+  if (is.null(pars)) {
+    pars <- coef(object)
+  }
+  n_pars <- length(pars)
+  #
+  # Threshold exceedances (values that lie above the threshold)
+  response_data <- object$exceedances
+  n <- length(response_data)
+  # Calculate the loglikelihood contributions
+  if (pars[2] <= 0) {
+    val <- -Inf
+  } else {
+    fn <- function(i) {
+      return(mev::egp.ll(xdat = response_data[i], thresh = object$thresh,
+                         par = pars, model = object$model))
+    }
+    val <- vapply(1:n, fn, 0.0)
+  }
+  # Return the usual attributes for a "logLik" object
+  attr(val, "nobs") <- nobs(object)
+  attr(val, "df") <- n_pars
+  class(val) <- "logLikVec"
+  return(val)
+}
