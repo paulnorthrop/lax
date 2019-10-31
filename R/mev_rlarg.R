bef0a28b23866a28b8a125384186c1ac9ce9042f
Paul Northrop <p.northrop@ucl.ac.uk>
2019-10-31 21:19:57 +0000

    logLikVec method for mev_rlarg

---
+++ R/mev_rlarg.R
@@ -0,0 +1,46 @@
+# ============================= mev::rlarg.fit ============================ #
+
+# logLikVec method for class mev_rlarg
+
+#' @export
+logLikVec.mev_rlarg <- function(object, pars = NULL, ...) {
+  if (!missing(...)) {
+    warning("extra arguments discarded")
+  }
+  # If the parameter estimates have not been provided in pars then extract
+  # them from the fitted object
+  if (is.null(pars)) {
+    pars <- coef(object)
+  }
+  n_pars <- length(pars)
+  #
+  response_data <- object$xdat
+  # If trans = FALSE then there are no covariates and object$data contains
+  # the response data
+  mu <- pars[1]
+  sigma <- pars[2]
+  xi <- pars[3]
+  # Calculate the loglikelihood contributions
+  if (any(sigma <= 0)) {
+    val <- -Inf
+  } else {
+    rlarg_loglik_vec <- function(x, mu, sigma, xi) {
+      logg <- apply(x, 2, revdbayes::dgev, loc = mu, scale = sigma,
+                    shape = xi, log = TRUE)
+      logG <- apply(x, 2, revdbayes::pgev, loc = mu, scale = sigma,
+                    shape = xi, log.p = TRUE)
+      logGmin <- revdbayes::pgev(min_response, loc = mu, scale = sigma,
+                                 shape = xi, log.p = TRUE)
+      loglik <- logGmin + rowSums(logg - logG, na.rm = TRUE)
+      return(loglik)
+    }
+    min_response <- apply(response_data, 1, min, na.rm = TRUE)
+    val <- rlarg_loglik_vec(x = response_data, mu = mu, sigma = sigma, xi = xi)
+  }
+  # Return the usual attributes for a "logLik" object
+  attr(val, "names") <- NULL
+  attr(val, "nobs") <- nobs(object)
+  attr(val, "df") <- n_pars
+  class(val) <- "logLikVec"
+  return(val)
+}
