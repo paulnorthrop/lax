b4d1cf4c3d04451c9e7f4891b1b735bd75d16bdc
Paul Northrop <p.northrop@ucl.ac.uk>
2019-11-01 22:24:02 +0000

    Add note

--- R/mev.R
+++ R/mev.R
@@ -10,6 +10,10 @@
 #'
 #' @inherit alogLik params references
 #' @details See \code{\link{alogLik}} for details.
+#'
+#' If \code{x} was returned from \code{\link[mev]{fit.pp}} then the data
+#' \code{xdat} supplied to \code{\link[mev]{fit.pp}} must contain \emph{all}
+#' the data, both threshold exceedances and non-exceedances.
 #' @return An object inheriting from class \code{"chandwich"}.  See
 #'   \code{\link[chandwich]{adjust_loglik}}.
 #'   \code{class(x)} is a vector of length 5. The first 3 components are
