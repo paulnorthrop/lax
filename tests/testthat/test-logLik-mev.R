cdb0430f9efb2a93d0f34aeb9bd87d83d1b14602
Paul Northrop <p.northrop@ucl.ac.uk>
2021-07-19 16:21:29 +0100

    Remove obsolete code: the mev object now contains these components

--- tests/testthat/test-logLik-mev.R
+++ tests/testthat/test-logLik-mev.R
@@ -54,9 +54,6 @@ if (requireNamespace("mev", quietly = TRUE)) {
   set.seed(1112019)
   x <- revdbayes::rgp(365 * 10, loc = 0, scale = 1, shape = 0.1)
   pfit <- mev::fit.pp(x, threshold = 1, npp = 365)
-  # (To do: delete the next two lines after new mev hits CRAN)
-  pfit$xdat <- x
-  pfit$npp <- 365
   temp <- pfit
   adj_pfit <- alogLik(pfit)
   class(temp) <- "laxmev_pp"
