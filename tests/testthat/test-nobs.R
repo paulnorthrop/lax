0173cfc89c1097b5b87c43c9a8007e24d7d79ec6
Paul Northrop <p.northrop@ucl.ac.uk>
2021-07-19 21:05:31 +0100

    Spacing

--- tests/testthat/test-nobs.R
+++ tests/testthat/test-nobs.R
@@ -34,7 +34,8 @@ if (requireNamespace("evd", quietly = TRUE)) {
   # evd::fextreme
   # An example from the evd::fextreme documentation
   uvdata <- evd::rextreme(100, qnorm, mean = 0.56, mlen = 365)
-  M3 <- evd::fextreme(uvdata, list(mean = 0, sd = 1), distn = "norm", mlen = 365)
+  M3 <- evd::fextreme(uvdata, list(mean = 0, sd = 1), distn = "norm",
+                      mlen = 365)
   test_that("evd::fextreme, nobs.evd() vs. length(response)", {
     testthat::expect_equivalent(nobs(M3), length(uvdata))
   })
